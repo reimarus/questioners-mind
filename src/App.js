fd0a76c9841d7ccb7af0333edbbd7e4f30a94dba
Ryan <roskvarek@gmail.com>
2024-07-19 22:51:36 +0000

    deeted extra header import in app.js.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
-import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import About from './pages/AboutMe';
